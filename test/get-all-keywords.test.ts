cf4ec3078ab486e01640ffcffaa7c1c7ea96dacc
Andrii Arkhipov <andrew.bishope@gmail.com>
2024-03-25 01:18:09 +0200

    Test cases - get all keywords

---
+++ test/get-all-keywords.test.ts
@@ -0,0 +1,29 @@
+import { FlashText } from '../src/flashtext';
+
+describe('Get All Keywords', () => {
+  it('should return all keywords (case insensitive)', () => {
+    const flashText = new FlashText();
+
+    flashText.addKeyword('JS', 'Javascript');
+    flashText.addKeyword('TS', 'Typescript');
+
+    const result = flashText.getAllKeywords();
+    expect(result).toEqual({
+      js: 'Javascript',
+      ts: 'Typescript',
+    });
+  });
+
+  it('should return all keywords (case sensitive)', () => {
+    const flashText = new FlashText(true);
+
+    flashText.addKeyword('JS', 'Javascript');
+    flashText.addKeyword('TS', 'Typescript');
+
+    const result = flashText.getAllKeywords();
+    expect(result).toEqual({
+      JS: 'Javascript',
+      TS: 'Typescript',
+    });
+  });
+});
